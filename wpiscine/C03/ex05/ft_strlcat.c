e53d36036f189cf044662f08314ee99a624aadb4
William <htetwaiyantoe.mmr@gmail.com>
2025-11-02 20:48:07 +0700

    C03

---
+++ wpiscine/C03/ex05/ft_strlcat.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: htoe <htoe@student.42bangkok.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/02 20:40:20 by htoe              #+#    #+#             */
+/*   Updated: 2025/11/02 20:46:46 by htoe             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	int	j;
+
+	i = 0;
+	while (dest[i])
+		i++;
+	j = 0;
+	while (i < size && src[j])
+		dest[i++] = src[j++];
+	dest[i] = '\0';
+	return (i);
+}

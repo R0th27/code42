9ae5bcd6c7b55f975b312762daeee4bd6afd5abb
William <htetwaiyantoe.mmr@gmail.com>
2025-11-03 03:40:12 +0700

    C06

---
+++ wpiscine/C06/ex03/ft_sort_params.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: htoe <htoe@student.42bangkok.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/03 03:16:02 by htoe              #+#    #+#             */
+/*   Updated: 2025/11/03 03:39:05 by htoe             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+static int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	int		j;
+	char	*temp;
+
+	i = 1;
+	while (i + 1 < argc)
+	{
+		if (ft_strcmp(argv[i], argv[i + 1]) > 0)
+		{
+			temp = argv[i];
+			argv[i] = argv[i + 1];
+			argv[i + 1] = temp;
+			i = 0;
+		}
+		i++;
+	}
+	i = 0;
+	while (i++ < argc - 1)
+	{
+		j = 0;
+		while (argv[i][j])
+			write(1, &argv[i][j++], 1);
+		write(1, "\n", 1);
+	}
+	return (0);
+}

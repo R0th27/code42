59eab6e7629f4f44571a1b253f7822194cd09a25
William <htetwaiyantoe.mmr@gmail.com>
2025-11-26 01:47:41 +0700

    GNL C H UC

--- wcore/ms1/get_next_line/get_next_line.c
+++ wcore/ms1/get_next_line/get_next_line.c
@@ -6,42 +6,98 @@
 /*   By: htoe <htoe@student.42bangkok.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 09:15:53 by htoe              #+#    #+#             */
-/*   Updated: 2025/11/13 23:07:14 by htoe             ###   ########.fr       */
+/*   Updated: 2025/11/26 01:40:24 by htoe             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+char	*left_over(char *cache)
 {
-	char		*str;
-	int			byte;
-	static char	buffer[BUFFER_SIZE];
+	size_t	i;
+	size_t	j;
+	char	*temp;
 
-	byte = read(fd, buffer, BUFFER_SIZE);
-	if (byte > 0)
-		str = buffer;
-	else
-		str =
-	printf("byte: %d\n", byte);
-	str = buffer;
+	i = 0;
+	while (cache[i] && cache[i] != '\n')
+		i++;
+	if (!cache[i])
+	{
+		free(cache);
+		return (NULL);
+	}
+	temp = (char *)malloc(sizeof(char) * (ft_strlen(cache) - i + 1));
+	if (!temp)
+		return (NULL);
+	j = 0;
+	while (cache[++i])
+		temp[j++] = cache[i];
+	temp[j] = '\0';
+	free(cache);
+	return (temp);
+}
+
+char	*extract_line(char *cache)
+{
+	size_t	len;
+	char	*str;
+
+	len = 0;
+	if (!cache[len])
+		return (NULL);
+	while (cache[len] && cache[len] != '\n')
+		len++;
+	if (cache[len] == '\n')
+		len++;
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	len = 0;
+	while (cache[len] && cache[len] != '\n')
+	{
+		str[len] = cache[len];
+		len++;
+	}
+	str[len] = cache[len];
+	if (cache[len])
+		str[++len] = '\0';
 	return (str);
 }
 
-int	main(void)
+char	*store_line(int fd, char *cache)
 {
-	int		fd1;
-	char	*str;
-	int		i;
+	int		byte;
+	char	*buffer;
 
-	fd1 = open("a.txt", O_RDONLY);
-	i = 0;
-	while (i < 5)
+	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	cache = ft_strjoin(cache, "");
+	byte = 1;
+	while (!ft_strchr(cache, '\n') && byte > 0)
 	{
-		str = get_next_line(fd1);
-		printf("str: %s\n", str);
-		i++;
+		byte = read(fd, buffer, BUFFER_SIZE);
+		if (byte == -1)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		buffer[byte] = '\0';
+		cache = ft_strjoin(cache, buffer);
 	}
-	close(fd1);
-	return (0);
+	free(buffer);
+	return (cache);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*cache;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	cache = store_line(fd, cache);
+	if (!cache)
+		return (NULL);
+	line = extract_line(cache);
+	cache = left_over(cache);
+	return (line);
 }
--- wcore/ms1/get_next_line/get_next_line.h
+++ wcore/ms1/get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: htoe <htoe@student.42bangkok.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 09:11:59 by htoe              #+#    #+#             */
-/*   Updated: 2025/11/13 09:13:45 by htoe             ###   ########.fr       */
+/*   Updated: 2025/11/26 01:37:06 by htoe             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,17 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 10
+#  define BUFFER_SIZE 1
 # endif
 
 # include <stdio.h>
-# include <stdlib.h>
 # include <fcntl.h>
 # include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strjoin(char *s1, const char *s2);
+size_t	ft_strlen(const char *str);
 
 #endif
